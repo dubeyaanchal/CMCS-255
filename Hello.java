3d31d7632f4d10aaab67e008e1b79f70e9dbcaba
dubeyaanchal <78099980+dubeyaanchal@users.noreply.github.com>
2021-01-27 12:08:52 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************
+* Java Program Name
+***********************************************
+* Aanchal Dubey
+* 01/27/2021
+* CMSC255-003
+**********************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Aanchal Dubey");
+		System.out.println("Hello World");
+		System.out.println("Lab 1");
+		System.out.println("01/27/2021");
+		System.out.println("CMSC255-003");
+		System.out.println();
+	}
+}
